8f53391c2e22290d0e5b8a752ac5025bafff8fec
Hasan-aga <careerforma@gmail.com>
2023-05-31 13:10:48 +0300

    update default config to match that of openresty image

--- platform/viewer/public/config/default.js
+++ platform/viewer/public/config/default.js
@@ -4,13 +4,19 @@ window.config = {
   servers: {
     dicomWeb: [
       {
-        name: 'My Orthanc',
-        wadoUriRoot: '/wado',
-        qidoRoot: '/dicom-web',
-        wadoRoot: '/dicom-web',
+        name: 'Orthanc',
+        wadoUriRoot: 'http://127.0.0.1/pacs/wado',
+        qidoRoot: 'http://127.0.0.1/pacs/dicom-web',
+        wadoRoot: 'http://127.0.0.1/pacs/dicom-web',
         qidoSupportsIncludeField: false,
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
+        enableStudyLazyLoad: true,
+        supportsFuzzyMatching: true,
+        // requestOptions: {
+        // undefined to use JWT + Bearer auth
+        // auth: 'orthanc:orthanc',
+        // },
       },
     ],
   },
